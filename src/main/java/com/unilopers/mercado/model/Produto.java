62f52f891ef2c7277c2dfee5adca4576f8dd9aed
LucasMarani <lucas.marani@edu.unifil.br>
2025-11-19 22:00:55 -0300

    ✨feat: Adição de variaveis, getters e setters para Produto.java

--- src/main/java/com/unilopers/mercado/model/Produto.java
+++ src/main/java/com/unilopers/mercado/model/Produto.java
@@ -1,5 +1,59 @@
 package com.unilopers.mercado.model;
+import java.util.Date;
 
 public class Produto {
+    private int idProduto;
+    private String nomeProduto;
+    private double precoProduto;
+    private int quantidadeEstoque;
+    private Date validade;
+    private int idCategoria;
 
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public Date getValidade() {
+        return validade;
+    }
+
+    public void setValidade(Date validade) {
+        this.validade = validade;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
 }
