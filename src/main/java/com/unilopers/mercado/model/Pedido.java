b73428b99b23d72f745b0a8b2c1f1fdbf6928fd1
Mariana Yumi Morishita Yokomizo <mariyumiyokomizo@gmail.com>
2025-11-20 16:52:11 -0300

    ✨ feat: implementação da entidade Pedido

--- src/main/java/com/unilopers/mercado/model/Pedido.java
+++ src/main/java/com/unilopers/mercado/model/Pedido.java
@@ -1,4 +1,38 @@
 package com.unilopers.mercado.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "pedido")  
 public class Pedido {
+
+ @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pedido")
+    private Long idPedido;
+
+    @Column(name = "preco_total", nullable = false)
+    private double precoTotal;
+
+    @Column(name = "tipo_pagamento", nullable = false, length = 50)
+    private String tipoPagamento;
+
+    @Column(nullable = false)
+    private boolean pago;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_cliente", nullable = false)
+    private Cliente cliente;
+
 }
+
+
