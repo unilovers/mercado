8560e865ff3685b4a2428831a9c8cb4bbb25d857
LucasMarani <lucas.marani@edu.unifil.br>
2025-11-23 16:54:32 -0300

    ✨ feat: implementação do ProdutoRepository.java

--- src/main/java/com/unilopers/mercado/repository/ProdutoRepository.java
+++ src/main/java/com/unilopers/mercado/repository/ProdutoRepository.java
@@ -1,5 +1,10 @@
 package com.unilopers.mercado.repository;
 
-public class ProdutoRepository {
+import com.unilopers.mercado.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Optional<Produto>findByNomeProduto(String nomeProduto);
 }
