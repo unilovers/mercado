fbc100d432b69924f4bbc635c2055b610bdbfdc4
Henrique Liuti <henrique.liuti5@gmail.com>
2025-11-25 17:26:01 -0300

    ✨feat: implementação do Item_pedido.java

--- src/main/java/com/unilopers/mercado/model/Item_pedido.java
+++ src/main/java/com/unilopers/mercado/model/Item_pedido.java
@@ -1,4 +1,28 @@
 package com.unilopers.mercado.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "item_pedido")
 public class Item_pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_pedido", nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_produto", nullable = false)
+    private Produto produto;
+
+    @Column(nullable = false)
+    private int quantidade;
+
+    @Column(name = "preco_unitario", nullable = false)
+    private double precoUnitario;
 }
