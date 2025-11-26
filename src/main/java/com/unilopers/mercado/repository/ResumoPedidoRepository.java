e49883b5cb9a558239842e432ac5c1912962af20
Mariana Yumi Morishita Yokomizo <mariyumiyokomizo@gmail.com>
2025-11-25 23:33:52 -0300

    ✨feat :implementação ResumoPedidoRepository

---
+++ src/main/java/com/unilopers/mercado/repository/ResumoPedidoRepository.java
@@ -0,0 +1,7 @@
+package com.unilopers.mercado.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.unilopers.mercado.model.ResumoPedido;
+
+public interface ResumoPedidoRepository extends JpaRepository<ResumoPedido, Long> {
+}
