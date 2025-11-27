9b9a1b1d37a870eabb6ea1f735c441e18401214b
Mariana Yumi <mariyumiyokomizo@gmail.com>
2025-11-26 23:26:26 -0300

    ♻️refactor: padronização do uso de Lombok @DaTa Item_pedido.java

--- src/main/java/com/unilopers/mercado/model/Item_pedido.java
+++ src/main/java/com/unilopers/mercado/model/Item_pedido.java
@@ -26,43 +26,4 @@ public class Item_pedido {
     @Column(name = "preco_unitario", nullable = false)
     private double precoUnitario;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Pedido getPedido() {
-        return pedido;
-    }
-
-    public void setPedido(Pedido pedido) {
-        this.pedido = pedido;
-    }
-
-    public Produto getProduto() {
-        return produto;
-    }
-
-    public void setProduto(Produto produto) {
-        this.produto = produto;
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public double getPrecoUnitario() {
-        return precoUnitario;
-    }
-
-    public void setPrecoUnitario(double precoUnitario) {
-        this.precoUnitario = precoUnitario;
-    }
 }
