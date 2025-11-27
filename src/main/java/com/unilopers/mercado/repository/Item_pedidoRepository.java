4eccb0392d2b0f68d7f17518b6f54134c55f698f
Mariana Yumi <mariyumiyokomizo@gmail.com>
2025-11-26 23:24:09 -0300

    ✨feat: adicionar método findByPedidoIdPedido

--- src/main/java/com/unilopers/mercado/repository/Item_pedidoRepository.java
+++ src/main/java/com/unilopers/mercado/repository/Item_pedidoRepository.java
@@ -1,9 +1,13 @@
 package com.unilopers.mercado.repository;
 
 import com.unilopers.mercado.model.Item_pedido;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface Item_pedidoRepository extends JpaRepository<Item_pedido, Long> {
+    List<Item_pedido> findByPedidoIdPedido(Long idPedido);
 }
